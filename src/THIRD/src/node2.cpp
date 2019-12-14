5ea0fd42bb69eacd6ff5897347b2bcebdf7af4ef
poker6037 <58728846+poker6037@users.noreply.github.com>
2019-12-14 12:14:18 +0530

    Add files via upload

---
+++ src/THIRD/src/node2.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+ image_transport::Publisher pub;
+char c;
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  { cv::Mat img=cv_bridge::toCvShare(msg, "bgr8")->image;
+    cv::imshow("2nd node preview",img);
+    cv::waitKey(30);
+    if(c == 'p')
+    pub.publish(msg);
+    else if(c =='s')
+    cv::imwrite("output.png",img);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{ 
+  ros::init(argc, argv, "node_2");
+  ros::NodeHandle nh;
+  cv::namedWindow("2nd node preview");
+  cv::startWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("cam", 1, imageCallback);
+  pub  = it.advertise("cam2", 1);    
+  std::cin>>c; 
+ros::spin();
+  cv::destroyWindow("2nd node preview");
+}
---
+++ src/THIRD/src/node3.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("3rd node preview", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "node_3");
+  ros::NodeHandle nh;
+  cv::namedWindow("3rd node preview");
+  cv::startWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("cam2", 1, imageCallback);
+  ros::spin();
+  cv::destroyWindow("3rd node preview");
+}
